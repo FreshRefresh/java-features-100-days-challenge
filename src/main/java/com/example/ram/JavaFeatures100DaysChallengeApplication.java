9fb3c8a6dc36f3f417279c5befb1df31a9359596
Ram Mittala <mittalas@amazon.com>
2022-10-06 18:06:51 -0700

    Init commit

---
+++ src/main/java/com/example/ram/JavaFeatures100DaysChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFeatures100DaysChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFeatures100DaysChallengeApplication.class, args);
+    }
+
+}
