738c3b4cee88d4fdbdf38f7ffea89f006234dd5f
Ram Mittala <mittalas@amazon.com>
2022-10-13 07:42:32 -0700

    Add log4j support

--- src/main/java/com/example/ram/controllers/HelloController.java
+++ src/main/java/com/example/ram/controllers/HelloController.java
@@ -1,15 +1,23 @@
 package com.example.ram.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/service")
+@Slf4j
 public class HelloController {
 
     @GetMapping("/name")
     public String getName() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+
         return "Hello World!";
     }
 
