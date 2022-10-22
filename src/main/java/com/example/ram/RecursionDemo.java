d80f5d3d12a77fe23efae4e27453c114d563e8d1
Ram Mittala <mittalas@amazon.com>
2022-10-22 12:43:08 -0700

    Recursion demo

---
+++ src/main/java/com/example/ram/RecursionDemo.java
@@ -0,0 +1,18 @@
+package com.example.ram;
+
+public class RecursionDemo {
+
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println("Sum of first 10 numbers = " + result);
+    }
+
+    private static int sum(int i) {
+
+        if (i > 0) {
+            return i + sum(i - 1);
+        } else {
+            return 0;
+        }
+    }
+}
